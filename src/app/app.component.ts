6fbcd988734b6a2072534c2801a579de80afc48c
Andrey Shalakhov <33981094+shalakhov-as@users.noreply.github.com>
2019-06-25 12:35:10 +0300

    upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+     
+class Item{
+    purchase: string;
+    done: boolean;
+    price: number;
+     
+    constructor(purchase: string, price: number) {
+  
+        this.purchase = purchase;
+        this.price = price;
+        this.done = false;
+    }
+}
+ 
+@Component({
+    selector: 'purchase-app',
+    templateUrl: './app.component.html'
+})
+export class AppComponent { 
+    items: Item[] = 
+    [
+        { purchase: "Хлеб", done: false, price: 15.9 },
+        { purchase: "Масло", done: false, price: 60 },
+        { purchase: "Картофель", done: true, price: 22.6 },
+        { purchase: "Сыр", done: false, price:310 }
+    ];
+    addItem(text: string, price: number): void {
+         
+        if(text==null || text.trim()=="" || price==null)
+            return;
+        this.items.push(new Item(text, price));
+    }
+}
